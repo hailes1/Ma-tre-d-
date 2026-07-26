2335835918db85d45538ee6f915d96f4aa94ce0a
Dagmawe Amare Haileslassie <dag.haileslassie@gmail.com>
2026-07-26 15:29:01 -0500

    Import Carbon Design System, create a global header that can manage notifications for now.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,43 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <cv-header>
+    <template #header-global>
+      <cv-header-global-action
+        @click="actionNotifications"
+      >
+        <Notification20 />
+      </cv-header-global-action>
+
+      <cv-header-global-action
+        aria-label="User avatar"
+        aria-controls="user-panel"
+        @click="actionUserAvatar"
+        label="Log in"
+        tip-position="bottom"
+        tip-alignment="center"
+      >
+        <UserAvatar20 v-if="loggedIn" />
+        <Login20 v-else />
+      </cv-header-global-action>
+
+    </template>
+  </cv-header>
+
+  <main id="main-content" class="app-content">
+    <div class="app-content__inner">
+      <router-view />
+    </div>
+  </main>
 </template>
 
-<style scoped></style>
+<script setup>
+import { ref } from 'vue'
+import {
+  CvHeader,
+  CvHeaderGlobalAction,
+} from '@carbon/vue'
+import { Login20, Notification20, UserAvatar20 } from '@carbon/icons-vue'
+
+const loggedIn = ref(false)
+
+const actionNotifications = () => {}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import CarbonVue from '@carbon/vue'
 
 import App from './App.vue'
 import router from './router'
+import './assets/main.scss'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(CarbonVue)
 
 app.mount('#app')
